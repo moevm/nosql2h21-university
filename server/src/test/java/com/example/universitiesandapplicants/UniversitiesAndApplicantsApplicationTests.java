7010d702773a5175ff05d8e7be70617c33538786
IvanShish <rayman.4@list.ru>
2021-11-29 23:06:10 +0300

    command line runner deleted

--- server/src/test/java/com/example/universitiesandapplicants/UniversitiesAndApplicantsApplicationTests.java
+++ server/src/test/java/com/example/universitiesandapplicants/UniversitiesAndApplicantsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.universitiesandapplicants;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class UniversitiesAndApplicantsApplicationTests {
 
     @Test
+    @Disabled
     void contextLoads() {
     }
 
